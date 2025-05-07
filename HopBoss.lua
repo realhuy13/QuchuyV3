b0b5ea3942ce917b28bace68f4d989797f04fa32
realhuy13 <huy434830@gmail.com>
2025-05-07 13:08:14 +0700

    Add files via upload

---
+++ HopBoss.lua
@@ -0,0 +1,91 @@
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
+
+       local Window = MakeWindow({
+         Hub = {
+         Title = "QucHuy Hop",
+         Animation = "Premium"
+         },
+        Key = {
+        KeySystem = true,
+        Title = "Key System",
+        Description = "",
+        KeyLink = "https://web.telegram.org/a/#-1002642640976",
+        Keys = {"huydepzai"},
+        Notifi = {
+        Notifications = true,
+        CorrectKey = "Đã Vô",
+       Incorrectkey = "Sai Key Rồi",
+       CopyKeyLink = "Nhập Key Vào"
+      }
+    }
+  })
+
+       MinimizeButton({
+       Image = "http://www.roblox.com/asset/?id=125996632115264",
+       Size = {60, 60},
+       Color = Color3.fromRGB(10, 10, 10),
+       Corner = true,
+       Stroke = false,
+       StrokeColor = Color3.fromRGB(255, 0, 0)
+      })
+      
+------ Tab
+     local Tab1o = MakeTab({Name = "Hop Boss"})
+     local Tab2o = MakeTab({Name = "Thông tin"})
+     
+------- BUTTON
+    
+    AddButton(Tab1o, {
+     Name = "Rip_indra (Wait Update)",
+    Callback = function()
+  end
+  })
+
+AddButton(Tab1o, {
+     Name = "Dough King (Wait Update)",
+    Callback = function()
+  end
+  })
+
+AddButton(Tab1o, {
+     Name = "Đảo bí ẩn (Wait Update)",
+    Callback = function()
+  end
+  })
+
+AddButton(Tab1o, {
+     Name = "Kiếm Thần Thoại (Wait Update)",
+    Callback = function()
+  end
+  })
+
+AddButton(Tab1o, {
+     Name = "Haki Thần Thoại (Wait Update)",
+    Callback = function()
+  end
+  })
+
+AddButton(Tab1o, {
+     Name = "Dark Black (Wait Update)",
+    Callback = function()
+  end
+  })
+
+AddButton(Tab1o, {
+     Name = "TOTS (Wait Update)",
+    Callback = function()
+  end
+  })
+
+AddButton(Tab1o, {
+     Name = "Full Mon (Wait Update)",
+    Callback = function()
+  end
+  })
+
+AddButton(Tab2o, {
+     Name = "Tiktok",
+    Callback = function()
+     https://www.tiktok.com/@huyne_7
+  end
+  })
